8ac24d0e43645ef6fd017a5056d311f4994a36fd
Akshola00 <akinniyishola07@gmail.com>
2025-06-22 03:00:05 +0100

    refactor: update schema and bindings for Dojo integration; introduce GameCounter interface and enhance Game and PoolPlayer models with field order definitions

--- client/src/dojo/bindings.ts
+++ client/src/dojo/bindings.ts
@@ -1,5 +1,6 @@
 import type { SchemaType as ISchemaType } from "@dojoengine/sdk";
 import { BigNumberish } from 'starknet';
+type WithFieldOrder<T> = T & { fieldOrder: string[] };
 
 export interface Game {
     id: BigNumberish;
@@ -32,6 +33,11 @@ export interface PoolPlayer {
     user_staked_status: boolean;
 }
 
+export interface GameCounter {
+    universal_id: BigNumberish;
+    current_game_id: BigNumberish;
+}
+
 export interface ProtocolStats {
     universal_id: BigNumberish;
     total_pools: BigNumberish;
@@ -57,3 +63,82 @@ export interface GamePlayers {
     game_id: BigNumberish;
     players: Array<string>;
 }
+
+export interface SchemaType extends ISchemaType {
+    flipnet: {
+        Game: WithFieldOrder<Game>,
+        PoolPlayer: WithFieldOrder<PoolPlayer>,
+        GameCounter: WithFieldOrder<GameCounter>,
+        ProtocolStats: WithFieldOrder<ProtocolStats>,
+        ClaimableReward: WithFieldOrder<ClaimableReward>,
+        OngoingPool: WithFieldOrder<OngoingPool>,
+        GamePlayers: WithFieldOrder<GamePlayers>,
+    },
+}
+
+export const schema: SchemaType = {
+    flipnet: {
+        Game: {
+            fieldOrder: ['id', 'num_players', 'num_heads', 'num_tails', 'total_stake_amount', 'total_heads_stake', 'total_tails_stake', 'status', 'is_live', 'created_at', 'starts_at', 'ends_at', 'duration', 'winning_option', 'manager_address', 'rewards_distributed', 'payout_rate'],
+            id: 0,
+            num_players: 0,
+            num_heads: 0,
+            num_tails: 0,
+            total_stake_amount: 0,
+            total_heads_stake: 0,
+            total_tails_stake: 0,
+            status: 0,
+            is_live: false,
+            created_at: 0,
+            starts_at: 0,
+            ends_at: 0,
+            duration: 0,
+            winning_option: null,
+            manager_address: "",
+            rewards_distributed: false,
+            payout_rate: 0,
+        },
+        PoolPlayer: {
+            fieldOrder: ['game_id', 'player_address', 'option', 'stake_amount', 'joined_at', 'claimed', 'reward_amount', 'user_staked_status'],
+            game_id: 0,
+            player_address: "",
+            option: null,
+            stake_amount: 0,
+            joined_at: 0,
+            claimed: false,
+            reward_amount: 0,
+            user_staked_status: false,
+        },
+        GameCounter: {
+            fieldOrder: ['universal_id', 'current_game_id'],
+            universal_id: 0,
+            current_game_id: 0,
+        },
+        ProtocolStats: {
+            fieldOrder: ['universal_id', 'total_pools', 'total_staked', 'total_bets', 'total_rewards', 'total_fees'],
+            universal_id: 0,
+            total_pools: 0,
+            total_staked: 0,
+            total_bets: 0,
+            total_rewards: 0,
+            total_fees: 0,
+        },
+        ClaimableReward: {
+            fieldOrder: ['game_id', 'player_address', 'amount', 'claimed'],
+            game_id: 0,
+            player_address: "",
+            amount: 0,
+            claimed: false,
+        },
+        OngoingPool: {
+            fieldOrder: ['universal_id', 'ongoing_status'],
+            universal_id: 0,
+            ongoing_status: false,
+        },
+        GamePlayers: {
+            fieldOrder: ['game_id', 'players'],
+            game_id: 0,
+            players: [],
+        },
+    },
+};
